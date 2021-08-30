a97e8655480f147f555522c711ff8e19423a8a95
Sebkd <sebkd@mail.ru>
2021-08-30 21:09:19 +0700

    Задание 6 выполнено

--- two_task6.py
+++ two_task6.py
@@ -8,6 +8,30 @@
 Если за 10 попыток число не отгадано, то вывести загаданное число
 """
 
+import random
+
+
+def func_tasksix(number_in, count):
+    """
+    Функция реализует простое сравнения введенного пользователем числа и
+    запрограммированным
+    :param number_in: загаданное число
+    :param count: количество попыток
+    """
+    print (f'Попытка: {count}')
+    answer = int (input ('Введите число от 0 до 100: '))
+    if count == 10 or answer == number_in:
+        if answer == number_in:
+            print ('Угадали!')
+        else:
+            print('Кол-во попыток закончилось')
+    else:
+        if answer > number_in:
+            print (f'Загаданное число меньше чем {answer}')
+        else:
+            print (f'Загаданное число больше чем {answer}')
+        func_tasksix (number_in, count + 1)
 
 
 if __name__ == '__main__':
+    func_tasksix (random.randint (0, 100), 1)
