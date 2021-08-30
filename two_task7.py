b66eb3f43d9b2adf081860188f4a481f855922c3
Sebkd <sebkd@mail.ru>
2021-08-30 21:22:10 +0700

    Задание 7 выполненно

--- two_task7.py
+++ two_task7.py
@@ -7,5 +7,20 @@
 """
 
 
+def func_taskseven(number):
+    """
+    Рекурсивная функция обратного подсчета вручную 1+2+...+n
+    :param number: количество элементов
+    :return: их сумма, если количество элементов равно 1, выводится 1
+    так как вызывать функцию не нужно
+    """
+    return number if number == 1 else number + func_taskseven (number - 1)
+
 
 if __name__ == '__main__':
+    try:
+        NATURAL_NUMBER = int (input ('Введите натуральное число: '))
+        print (func_taskseven (NATURAL_NUMBER))
+        print (f'{int (NATURAL_NUMBER * (NATURAL_NUMBER + 1) / 2)}')
+    except ValueError:
+        print ('Вы ввели не число')
