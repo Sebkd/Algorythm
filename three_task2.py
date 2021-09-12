0c4d6dc49b1ba5bcce24e36f46db8304a51d3671
Sebkd <sebkd@mail.ru>
2021-09-12 20:11:41 +0700

    final task2

--- three_task2.py
+++ three_task2.py
@@ -7,5 +7,14 @@
 т.к. именно в этих позициях первого массива стоят четные числа
 """
 
+import random
 
 if __name__ == '__main__':
+    my_list = [number for number in range(21)]
+    random.shuffle(my_list)
+    print(my_list)
+    list_b = []
+    for index, number in enumerate(my_list):
+        if number % 2 == 0 and number != 0:
+            list_b.append(index)
+    print(list_b)
