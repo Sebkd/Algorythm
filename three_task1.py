5d9bf402c5814628c3b44a5b571de78bd591e62c
Sebkd <sebkd@mail.ru>
2021-09-12 19:39:56 +0700

    Final task3
    Добавил второй вариант решения 1 задания через заполнение списка сразу с условием выборки

--- three_task1.py
+++ three_task1.py
@@ -5,14 +5,31 @@
 сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
 """
 
-def func_count(number, *args):
-    my_temp = list(*args)
-    return len(list(filter(lambda x: x % number == 0, my_temp)))
+
+def func_count(number_in, *args):
+    """
+    Функция подсчета кратности итерируемого объекта определенному числу
+    :param number: кратное число
+    :param args: итерируемый объект
+    :return: возвращает кол-во вхождений
+    """
+    my_temp = list (*args)
+    return len (list (filter (lambda x: x % number_in == 0, my_temp)))
+
 
 if __name__ == '__main__':
+    '''Первый способ: заполнение, подсчет через функцию'''
     my_list = [number for number in range(2, 100)]
     print(my_list)
     for number in range (2, 10):
-        print(f'Для числа {number} кол-во кратных'
-              f' чисел в списке от 2 до 99'
-              f' равно: {func_count(number, my_list)}')
+        print (f'Для числа {number} кол-во кратных'
+               f' чисел в списке от 2 до 99'
+               f' равно: {func_count (number, my_list)}')
+    print('Второй способ')
+    '''Второй способ: заполнение, подсчет и вывод, сразу в процессе
+    заполнения списка'''
+    for multiple_number in range (2, 10):
+        print(f'Для числа {multiple_number} кол-во кратных'
+               f' чисел в списке от 2 до 99'
+               f' равно: '
+              f'{len([number for number in range(2, 100) if number % multiple_number == 0])}')
