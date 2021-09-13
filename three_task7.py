c3c89baf58ea7e282e4a2afbe868b7e079e18207
Sebkd <sebkd@mail.ru>
2021-09-13 20:52:44 +0700

    final task7

--- three_task7.py
+++ three_task7.py
@@ -6,7 +6,16 @@
 так и различаться
 """
 
+import random
 
 
 if __name__ == '__main__':
-
+    my_list = [random.randint (10, 100) for number in range (100)]
+    print (my_list)
+    sort_list = []
+    sort_list.extend(my_list)
+    sort_list.sort()
+    print(f'Два наименьших элемента списка 1) "{sort_list[0]}",'
+          f'с индексом "{my_list.index(sort_list[0])}", '
+          f'2) "{sort_list[1]}",'
+          f'с индексом "{my_list.index(sort_list[1])}"')
