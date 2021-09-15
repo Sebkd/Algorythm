31cc6a7c9dc86954d5582ef8d9a226c74831b94e
Sebkd <sebkd@mail.ru>
2021-09-15 23:16:50 +0700

    final task2

--- five_task2.py
+++ five_task2.py
@@ -10,9 +10,42 @@
 """
 
 
+def hex_to_int(*args):
+    """
+    Перевод значения 16-ричного в 10-ный [] + проверка что число в hex формате
+    :param args: типа A2
+    :return: [162]
+    """
+    temp_number = hex (int (''.join (list (*args)), 16))
+    return int (''.join (list (*args)), 16)
+
+
+def add_hex(*args):
+    """
+    Сложение 2-х 16-ричных чисел
+    :param args: десятичное число
+    :return: типа ['A','2','F']
+    """
+    return list (hex (args[0] + args[1]).upper ())[2:]
+
+
+def mult_hex(*args):
+    """
+    Умножение 2-х 16-ричных чисел
+    :param args: десятичное число
+    :return: типа ['A','2','F']
+    """
+    return list (hex (args[0] * args[1]).upper ())[2:]
+
 
 if __name__ == '__main__':
-    number_a = list(input('Введите первое 16-ричное число: '))
-    number_b = list (input ('Введите первое 16-ричное число: '))
-    print(f'{number_a},\n{number_b}\n')
-    
+    try:
+        number_a = list (input ('Введите первое 16-ричное число: '))
+        FIRST_HEX = hex_to_int (number_a)
+        number_b = list (input ('Введите второе 16-ричное число: '))
+        SECOND_HEX = hex_to_int (number_b)
+        print (f'Первое число: {number_a}\nВторое число: {number_b}\n')
+        print (f'Сложение этих чисел: {add_hex (FIRST_HEX, SECOND_HEX)}')
+        print (f'Умножение этих чисел: {mult_hex (FIRST_HEX, SECOND_HEX)}\n')
+    except ValueError:
+        print('Введено не 16-ричное число')
