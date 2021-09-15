f833b8ebdda88258af7d32d1a32029425056041c
Sebkd <sebkd@mail.ru>
2021-09-15 21:22:29 +0700

    continue task1

--- five_task1.py
+++ five_task1.py
@@ -8,8 +8,41 @@
 вывести наименования предприятий, чья прибыль ниже среднего
 """
 
-
-
-
+import collections
+from random import randint
 
 if __name__ == '__main__':
+    try:
+        Company = collections.namedtuple ('Company', ['quarter1',
+                                                      'quarter2',
+                                                      'quarter3',
+                                                      'quarter4',
+                                                      'avg_profit'])
+        book_of_company = {}
+        number_of_company = int (input ('Введите количество компаний: '))
+        manual_or_auto = 
+        for number in range (1, number_of_company + 1):
+            name = input (str (number) + '-ая компания ')
+            profit = [0] * 5
+            for value in range (4):
+                profit[value] = int (input ('Введите прибыль за ' + str (value + 1) + ' квартал: '))
+                profit[4] += profit[value]
+            profit[4] /= 4
+            book_of_company[name] = Company(
+                quarter1 = profit[0],
+                quarter2 = profit[1],
+                quarter3 = profit[2],
+                quarter4 = profit[3],
+                avg_profit = profit[4]
+            )
+        total_avg_profit = 0
+        for name, profit in book_of_company.items():
+            print(f'Компания {name}, средняя прибыль {profit[4]}')
+            total_avg_profit += profit[4]
+        print(f'Средняя прибыль (за год для всех компаний): {total_avg_profit} ')
+
+
+
+
+    except ValueError:
+        print ('Вы ввели не число')
