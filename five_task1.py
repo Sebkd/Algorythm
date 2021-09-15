38ff9209d74623f38a31da9fcb36b8503021940f
Sebkd <sebkd@mail.ru>
2021-09-15 21:46:44 +0700

    final lesson5 task1

--- five_task1.py
+++ five_task1.py
@@ -20,29 +20,49 @@ if __name__ == '__main__':
                                                       'avg_profit'])
         book_of_company = {}
         number_of_company = int (input ('Введите количество компаний: '))
-        manual_or_auto = 
-        for number in range (1, number_of_company + 1):
-            name = input (str (number) + '-ая компания ')
-            profit = [0] * 5
-            for value in range (4):
-                profit[value] = int (input ('Введите прибыль за ' + str (value + 1) + ' квартал: '))
-                profit[4] += profit[value]
-            profit[4] /= 4
-            book_of_company[name] = Company(
-                quarter1 = profit[0],
-                quarter2 = profit[1],
-                quarter3 = profit[2],
-                quarter4 = profit[3],
-                avg_profit = profit[4]
-            )
-        total_avg_profit = 0
-        for name, profit in book_of_company.items():
-            print(f'Компания {name}, средняя прибыль {profit[4]}')
-            total_avg_profit += profit[4]
-        print(f'Средняя прибыль (за год для всех компаний): {total_avg_profit} ')
-
-
-
-
+        manual_or_auto = input('Будете вручную вводить? (Y / N) ').isupper()
+        if manual_or_auto == 'Y':
+            for number in range (1, number_of_company + 1):
+                NAME = input (str (number) + '-ая компания ')
+                profit = [0] * 5
+                for value in range (4):
+                    profit[value] = int (input ('Введите прибыль '
+                                                'за ' + str (value + 1) + ' квартал: '))
+                    profit[4] += profit[value]
+                profit[4] /= 4
+                book_of_company[NAME] = Company(
+                    quarter1 = profit[0],
+                    quarter2 = profit[1],
+                    quarter3 = profit[2],
+                    quarter4 = profit[3],
+                    avg_profit = profit[4]
+                )
+        else:
+            for number in range (1, number_of_company + 1):
+                NAME = str (number) + '-ая компания '
+                profit = [0] * 5
+                for value in range (4):
+                    profit[value] = randint(100, 1000)
+                    profit[4] += profit[value]
+                profit[4] /= 4
+                book_of_company[NAME] = Company(
+                    quarter1 = profit[0],
+                    quarter2 = profit[1],
+                    quarter3 = profit[2],
+                    quarter4 = profit[3],
+                    avg_profit = profit[4]
+                )
+        TOTAL_AVG_PROFIT = 0
+        for NAME, profit in book_of_company.items():
+            print(f'Компания "{NAME}", средняя прибыль {profit[4]:.2f}')
+            TOTAL_AVG_PROFIT += profit[4] / len(book_of_company)
+        print(f'\nСредняя прибыль (за год для всех компаний): {TOTAL_AVG_PROFIT:.2f} \n')
+        for NAME, profit in book_of_company.items():
+            if profit[4] > TOTAL_AVG_PROFIT:
+                print(f'Компания "{NAME}" имеет прибыль выше средней ')
+        print('\n')
+        for NAME, profit in book_of_company.items():
+            if profit[4] < TOTAL_AVG_PROFIT:
+                print(f'Компания "{NAME}" имеет прибыль ниже средней ')
     except ValueError:
         print ('Вы ввели не число')
