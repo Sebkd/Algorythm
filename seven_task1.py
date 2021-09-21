74f3495dd40771004be2f225fefdd959fba70af5
Sebkd <sebkd@mail.ru>
2021-09-21 21:11:25 +0700

    final task1

--- seven_task1.py
+++ seven_task1.py
@@ -9,13 +9,45 @@
 """
 
 from random import randint
+from timeit import timeit, default_timer
+
 
 def bubble_sort(*args):
-    temp_list = list(args)
-    print(temp_list)
-    pass
+    """
+    Пузырьковая сортировка
+    :param args: несортированный массив
+    :return: сортированный массив
+    """
+    temp_list = list (args)
+    for index in range (len (temp_list) - 1):
+        for j in range (len (temp_list) - index - 1):
+            if temp_list[j] > temp_list[j + 1]:
+                temp_list[j], temp_list[j + 1] = temp_list[j + 1], temp_list[j]
+    return temp_list
+
+
+def bubble_sort_two(*args):
+    temp_list = list (args)
+    swap_signal = True
+    total = 0
+    while swap_signal:
+        swap_signal = False
+        for j in range (len (temp_list) - total - 1):
+            if temp_list[j] > temp_list[j + 1]:
+                temp_list[j], temp_list[j + 1] = temp_list[j + 1], temp_list[j]
+                swap_signal = True
+        total += 1
+    return temp_list
 
 
 if __name__ == '__main__':
-    my_list = [randint(-101, 101) for _ in range(200)]
-    bubble_sort(*my_list)
+    unsort_list = [randint (-100, 100) for _ in range (200)]
+    sort_list = bubble_sort (*unsort_list)
+    sort_opt_list = bubble_sort_two (*unsort_list)
+    print (f'Длина несортированного списка {len (unsort_list)}, список: {unsort_list}')
+    start_time = default_timer ()
+    print (f'Длина cортированного списка {len (sort_list)}, список: {sort_list}\n'
+           f'Время выполнения: {default_timer() - start_time}')
+    start_time = default_timer ()
+    print (f'Длина сортированного опти списка {len (sort_opt_list)}, список: {sort_opt_list}\n'
+           f'Время выполнения: {default_timer() - start_time}')
