ae65f0150e81f90b6e922b5d2b928e9268939f46
Sebkd <sebkd@mail.ru>
2021-09-23 21:11:23 +0700

    tea break

--- eleven_task2.py
+++ eleven_task2.py
@@ -4,6 +4,14 @@
 Закодируйте любую строку из трех слов по алгоритму Хаффмана
 """
 
+import heapq
+from collections import Counter
+from collections import namedtuple
+
+class Node(namedtuple("Node", ["left", "right"])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc + "0")
+        self.right.walk(code, acc + "1")
 
 
 if __name__ == '__main__':
